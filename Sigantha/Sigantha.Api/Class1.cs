f8129205af523dd900f53c6160a081fb14d33cda
tjpmaris <tjpmaris@gmail.com>
2020-02-01 08:44:29 -0600

    Feat: Add api project

---
+++ Sigantha/Sigantha.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sigantha.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sigantha/Sigantha.Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace Sigantha.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
